452b0a2fcf60429506908864b83c2bda548e3915
WalterLWY <lau_walter77@hotmail.com>
2023-12-15 16:45:28 -0700

    Route.js, lib/stripe updated

--- app/components/card/SingleProductCard.jsx
+++ app/components/card/SingleProductCard.jsx
@@ -1,8 +1,9 @@
 import Link from 'next/link'
 import Image from 'next/image'
+import { BuyNow } from '../buynowform/BuyNowForm'
 
 
-function SingleProductCard ({product_name, price, image_url, description, uid}){
+function SingleProductCard ({product_name, price, image_url, description, uid, priceId}){
     return (
            <aside className='m-10 p-10 bg-green-200'>
             <h2 className='p-1 font-bold text-lg'>{product_name}</h2>
@@ -10,8 +11,8 @@ function SingleProductCard ({product_name, price, image_url, description, uid}){
             <p className='p-1 font-bold'>Price: {price}</p>
             <p className='p-1 font-bold'>UID: {uid}</p>
             <p className='p-1'>Description: {description}</p>
+            <BuyNow priceId={priceId}/>
             <Link className='text-blue-600 font-semibold' href={`/products`}>Back to Products Page</Link>
-  
            </aside>
     )
   }
